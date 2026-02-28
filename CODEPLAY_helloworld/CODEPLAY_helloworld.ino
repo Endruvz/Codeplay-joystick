0710fc4d0ffeaa9d887c738c9d07c2c83c2d68cd
Jakov <jakovblaskovic2@gmail.com>
2026-02-28 11:14:01 +0100

    t

--- CODEPLAY_helloworld/CODEPLAY_helloworld.ino
+++ CODEPLAY_helloworld/CODEPLAY_helloworld.ino
@@ -5,12 +5,21 @@
 
 void setup() {
   
-  digitalRead(INPUT_PULLUP)
+  pinMode(INPUT_PULLUP, Butt1);
+  pinMode(INPUT_PULLUP, Butt2);
+  pinMode(INPUT_PULLUP, Butt3);
+  pinMode(INPUT_PULLUP, Butt4);
 
 Serial.begin(115200);
 Serial.println("Dobar pocetak za Codeplay!");
 }
 void loop() {
 delay(1000);
-Serial.println("Radi");
+
+
+Serial.println(digitalRead(Butt1));
+Serial.println(digitalRead(Butt2));
+Serial.println(digitalRead(Butt3));
+Serial.println(digitalRead(Butt4));
+
 }
