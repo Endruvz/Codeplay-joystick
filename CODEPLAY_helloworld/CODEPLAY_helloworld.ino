67eb047d5add9861f31ecf61d13c813bdd9259ff
Jakov <jakovblaskovic2@gmail.com>
2026-02-28 11:08:20 +0100

    t

--- CODEPLAY_helloworld/CODEPLAY_helloworld.ino
+++ CODEPLAY_helloworld/CODEPLAY_helloworld.ino
@@ -1,4 +1,12 @@
+#define Butt1 41
+#define Butt2 39
+#define Butt3 40
+#define Butt4 14
+
 void setup() {
+  
+  digitalRead(INPUT_PULLUP)
+
 Serial.begin(115200);
 Serial.println("Dobar pocetak za Codeplay!");
 }
