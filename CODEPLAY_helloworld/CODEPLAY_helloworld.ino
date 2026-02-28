6126a70f8db851907ab7d3a6d52d2a14afcdda45
Jakov <jakovblaskovic2@gmail.com>
2026-02-28 15:20:06 +0100

    Dodan xtask

--- CODEPLAY_helloworld/CODEPLAY_helloworld.ino
+++ CODEPLAY_helloworld/CODEPLAY_helloworld.ino
@@ -22,6 +22,73 @@ int deadZone = 5;
 #define PinLed 2
 #define NUM_LEDS 1
 CRGB led[NUM_LEDS];
+static uint8_t hue;
+
+TaskHandle_t SensTaskHandle = NULL;
+TaskHandle_t ReceiveTaskHandle = NULL;
+
+
+void SensTask(void *parameter){
+  for(;;){
+    int ValX = map(analogRead(PinX), 0, 4095, -100, 100);
+    int ValY = map(analogRead(PinY), 0, 4095, -100, 100);
+
+    if (abs(ValX) <= deadZone){
+      ValX = 0;
+    }
+
+    if (abs(ValY) <= deadZone){
+      ValY = 0;
+    }
+  
+
+    int gumbi = 0;
+    gumbi = gumbi | digitalRead(Butt1) | digitalRead(Butt2)*2 | digitalRead(Butt3)*4 | digitalRead(Butt4)*8; 
+
+
+    joystick.buttons = gumbi;
+
+    joystick.joy_x = ValX;
+    joystick.joy_y = ValY;
+
+    Serial.write((uint8_t*)&joystick, sizeof(joystick));
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+void ReceiveTask(void *parameter){
+  for(;;){
+  led[0] = CHSV(hue++,255,255);
+  FastLED.show();
+  vTaskDelay(33 / portTICK_PERIOD_MS);
+  if (Serial.available()) {
+    int16_t indata = Serial.read();
+    if (indata == 0x01){
+      //coin
+    }
+    else if (indata == 0x02){
+      //heal
+
+    }
+    else if (indata == 0x03){
+      //kill
+
+    }
+    else if (indata == 0x04){
+      //damage
+
+    }
+    else if (indata == 0x05){
+      //death
+
+    }
+
+
+
+  }
+
+}
+}
 
 void setup() {
   pinMode(Butt1, INPUT_PULLUP);
@@ -38,47 +105,32 @@ void setup() {
 
 Serial.begin(115200);
 Serial.println("Serial Start!");
-}
 
+//startup sound 
+
+
+ xTaskCreatePinnedToCore(
+    SensTask,         // Task function
+    "SensTask",       // Task name
+    10000,             // Stack size (bytes)
+    NULL,              // Parameters
+    1,                 // Priority
+    &SensTaskHandle,  // Task handle
+    1                  // Core 1
+  );
+
+
+ xTaskCreatePinnedToCore(
+    ReceiveTask,         // Task function
+    "ReceiveTask",       // Task name
+    10000,             // Stack size (bytes)
+    NULL,              // Parameters
+    1,                 // Priority
+    &ReceiveTaskHandle,  // Task handle
+    0                  // Core 1
+  );
+}
 
 void loop() {
-delay(10);
-
-int ValX = map(analogRead(PinX), 0, 4095, -100, 100);
-int ValY = map(analogRead(PinY), 0, 4095, -100, 100);
-
-if (abs(ValX) <= deadZone){
-  ValX = 0;
-}
-
-if (abs(ValY) <= deadZone){
-  ValY = 0;
-}
-
-
-int gumbi = 0;
-gumbi = gumbi | digitalRead(Butt1) | digitalRead(Butt2)*2 | digitalRead(Butt3)*4 | digitalRead(Butt4)*8; 
-
-
-joystick.buttons = gumbi;
-
-joystick.joy_x = ValX;
-joystick.joy_y = ValY;
-
-Serial.write((uint8_t*)&joystick, sizeof(joystick));
-
-
-
-/*
-Serial.println("##################");
-Serial.print("Butt1 ");
-Serial.println(digitalRead(Butt1));
-Serial.print("Butt2 ");
-Serial.println(digitalRead(Butt2));
-Serial.print("Butt3 ");
-Serial.println(digitalRead(Butt3));
-Serial.print("Butt4 ");
-Serial.println(digitalRead(Butt4));
-*/
 
 }
